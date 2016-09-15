ab12c6fe498b64f8c92a05160aba5a8186450ff1
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-09-15 02:03:02 -0400

    Added tests to fillParcellation
    There is a strange behavior with sample. If you use sample(10,1),
    it really runs sample(1:10,1).

--- R/fillParcellation.R
+++ R/fillParcellation.R
@@ -8,7 +8,11 @@ fillParcellation <- function(parcellation, shape.mat = neighborShape_Box27()){
       shape.mat = shape.mat)
     
     vec <- sapply(res, function(x){
-      if(any(x != 0)){return(sample(x[x!=0],1))} else {return(0)}
+      if(any(x != 0)){
+        x <- x[x!=0]
+        if(length(x) == 1) x <- rep(x,2)
+        return(sample(x,1)) #written to deal with sample(integer, size)
+      } else {return(0)}
     })
     
     parcellation$partition[empty.voxel[vec != 0]] <- vec[vec != 0]
--- tests/testthat/test_fillParcellation.R
+++ tests/testthat/test_fillParcellation.R
@@ -8,6 +8,54 @@ test_that("it works on a simple singular-parcel case", {
   res <- fillParcellation(parcellation)
   
   expect_true(class(res) == "BrcParcellation")
-  expect_true(all(res$parcellation$dim3d == rep(3,3)))
-  expect_true(all(res$parcellation$partition == 1))
+  expect_true(all(res$dim3d == rep(3,3)))
+  expect_true(all(res$partition == 1))
+})
+
+test_that("it works on seperate block parcellation", {
+  mat <- array(0, rep(5,3))
+  mat[1:3, 1:3, 1:3] <- 1
+  mat[4:5, 4:5, 4:5] <- 2
+  parcellation <- brcbase::buildBrcParcellation(mat)
+  
+  set.seed(10)
+  res <- fillParcellation(parcellation)
+  
+  expect_true(class(res) == "BrcParcellation")
+  expect_true(all(res$parcellation$dim3d == rep(5,3)))
+  
+  idx1 <- which(parcellation$partition == 1)
+  expect_true(all(res$partition[idx1] == 1))
+  
+  idx2 <- which(parcellation$partition == 2)
+  expect_true(all(res$partition[idx2] == 2))
+  
+  idx1.mat <- matrix(c(1,1,4, 1,2,4, 1,3,4, 1,4,4,
+                       2,1,4, 2,2,4, 2,3,4, 2,4,4,
+                       3,1,4, 3,2,4, 
+                       1,4,1, 1,4,2, 1,4,3, 
+                       2,4,1, 2,4,2, 2,4,3,
+                       3,4,1, 3,4,2,
+                       4,1,1, 4,1,2, 4,1,3, 4,1,4,
+                       4,2,1, 4,2,2, 4,2,3, 4,2,4,
+                       4,3,1, 4,3,2, 
+                       1,1,5, 1,2,5, 1,3,5, 1,4,5,
+                       2,1,5, 
+                       3,1,5, 
+                       1,5,1, 1,5,2, 1,5,3,
+                       2,5,1, 2,5,2, 
+                       3,5,1, 
+                       5,1,1, 5,1,2, 5,1,3, 5,1,4,
+                       5,2,1, 
+                       5,3,1), ncol = 3, byrow = T)
+  idx2.mat <- matrix(c(5,4,3, 4,5,3,
+                       4,3,5, 3,4,5, 
+                       4,5,3, 3,5,4,
+                       5,4,3, 5,3,4,
+                       5,5,3, 5,3,5, 3,5,5), ncol = 3, byrow = T)
+  
+  idx1.vec <- apply(idx1.mat, 1, brcbase::voxel3DToIdx, dim3d = rep(5,3))
+  idx2.vec <- apply(idx2.mat, 1, brcbase::voxel3DToIdx, dim3d = rep(5,3))
+  expect_true(all(res$partition[idx1.vec] == 1))
+  expect_true(all(res$partition[idx2.vec] == 2))
 })
