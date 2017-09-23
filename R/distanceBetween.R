ccae1834611cf4defc8111a4a1be94c04fabd779
klin1234@gmail.com <klin1234@gmail.com>
2017-09-22 22:20:51 -0400

    Added documentation for distanceBetween

--- R/distanceBetween.R
+++ R/distanceBetween.R
@@ -1,3 +1,19 @@
+#' Compute the distance between parcels in a parcellation
+#' 
+#' This function computes the distance between parcels in a given parcellation.
+#' 
+#' The distance between two parcels is passed into this function as another function,
+#' such as \code{distance_manhattan} for example, as the \code{dist} argument.
+#' The \code{dist} function must be able to take two matrices, each with 
+#' 3 rows, denoting the 3-dimensional indices (locations) of all the voxels in
+#' each of the two parcels. 
+#'
+#' @param parcellation a \code{BrcParcellation} object
+#' @param dist a distance function
+#' @param ... additional arguments to be passed into \code{dist} (optional)
+#'
+#' @return a \code{dist} object
+#' @export
 distanceBetween <- function(parcellation, dist = distance_manhattan, ...){
   .is.BrcParcellation(parcellation)
   
@@ -15,6 +31,17 @@ distanceBetween <- function(parcellation, dist = distance_manhattan, ...){
   stats::as.dist(mat)
 }
 
+#' A distance function between to matrices (Manhattan distance)
+#' 
+#' The input to this function are two matrices with the same number of rows.
+#' This function computes the minimum
+#' distance between any column in \code{mat1} to any column in \code{mat2}.
+#'
+#' @param mat1 a matrix
+#' @param mat2 a matrix
+#'
+#' @return a numeric
+#' @export
 distance_manhattan <- function(mat1, mat2){
   n1 <- ncol(mat1)
   n2 <- ncol(mat2)
