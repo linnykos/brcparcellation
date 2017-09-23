26181494c548f1633dcc2589bda5ab283ee1bf45
klin1234@gmail.com <klin1234@gmail.com>
2017-09-22 22:11:32 -0400

    Added documentation for createMask

--- R/createMask.R
+++ R/createMask.R
@@ -1,3 +1,17 @@
+#' Create a mask from an existing parcellation
+#' 
+#' This function takes in a \code{BrcParcellation} object and returns
+#' a new \code{BrcParcellation} object where all the voxels assigned
+#' to any parcels in the input parcellation are now assigned to parcel 1.
+#' 
+#' This is useful if the user wants to make a mask, so downstream, the user
+#' can zero-out all the voxels not in the parcellation for a region-of-interest
+#' (RoI) study.
+#'
+#' @param parcellation  a \code{BrcParcellation} object
+#'
+#' @return a \code{BrcParcellation} object
+#' @export
 createMask <- function(parcellation){
   .is.BrcParcellation(parcellation)
   
